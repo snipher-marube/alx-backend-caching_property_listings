2b4e3d86181e286a892e8b8540d5a117ef35a113
Snipher Marube <sniphermarube@gmail.com>
2025-07-17 14:14:08 +0300

    listing

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_redis',
-    'properties',  # Custom app for property listings
+    'properties',  
 ]
 
 MIDDLEWARE = [
